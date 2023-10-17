ba1cd77b9e52848660384a349a81f7e5986f9349
nono <noamcurely@gmail.com>
2023-10-17 05:51:09 +0200

    Connection a la db info dans le .env

---
+++ Back/Config/Database.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv").config();
+
+const sequelize = new Sequelize({
+    database: process.env.DB,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    dialect: "mysql",
+    host: "localhost"
+});
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error + "Premiere connection db");
+});
+
+module.exports = sequelize;
